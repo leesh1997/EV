18deb129cf03e4e92e55d6128d29d977e3ef4361
Jeanapril <95842409+Jeanapril@users.noreply.github.com>
2022-04-11 10:26:07 +0900

    palette 기능 구현 ing..... by 지은

--- src/main/java/kr/ev/ev/PaletteController.java
+++ src/main/java/kr/ev/ev/PaletteController.java
@@ -1,13 +1,29 @@
 package kr.ev.ev;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.ev.model.PaletteMapper;
+import kr.ev.model.PaletteVO;
+
+
+
 @Controller
 public class PaletteController {
+	
+	@Inject
+	private PaletteMapper mapper;
+	
 	@RequestMapping("/palette.do")
 	public String Main(Model model) {
+		List<PaletteVO> list = mapper.paletteColors(); 
+		model.addAttribute("list", list);
 		return "palette";
 	}
+	
 }
